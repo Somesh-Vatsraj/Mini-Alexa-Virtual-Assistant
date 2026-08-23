a70a5f571e4a354538181c9b0065a764319c3080
Somesh Vatsraj <someshsoftwareengineer@gmail.com>
2026-08-23 10:18:59 +0530

    Create jokes.js

---
+++ src/data/jokes.js
@@ -0,0 +1,7 @@
+export const jokes = [
+  "Why do programmers prefer dark mode? Because light attracts bugs!",
+  "There are 10 types of people in the world: those who understand binary, and those who don't.",
+  "Debugging is like being the detective in a crime movie where you are also the murderer.",
+  "Why do Java developers wear glasses? Because they don't C#.",
+  "A SQL query walks into a bar, walks up to two tables and asks: 'Can I join you?'"
+];
