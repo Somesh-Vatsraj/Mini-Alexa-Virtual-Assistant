10aa91c6d48b40ce10995cf42eb2eef073fcc951
Somesh Vatsraj <someshsoftwareengineer@gmail.com>
2026-08-23 10:18:36 +0530

    Create data/quotes.js

---
+++ src/data/quotes.js
@@ -0,0 +1,7 @@
+export const quotes = [
+  { quote: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" },
+  { quote: "It always seems impossible until it's done.", author: "Nelson Mandela" },
+  { quote: "Do what you can, with what you have, where you are.", author: "Theodore Roosevelt" },
+  { quote: "Success is not final; failure is not fatal: It is the courage to continue that counts.", author: "Winston Churchill" },
+  { quote: "Act as if what you do makes a difference. It does.", author: "William James" }
+];
