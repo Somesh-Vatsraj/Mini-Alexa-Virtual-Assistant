06edef06c1d9b35d3bae3e274a79377686092e87
Somesh Vatsraj <someshsoftwareengineer@gmail.com>
2026-08-23 10:19:15 +0530

    Create facts.js

---
+++ src/data/facts.js
@@ -0,0 +1,7 @@
+export const facts = [
+  "Honey never spoils. Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still edible.",
+  "Octopuses have three hearts and blue blood.",
+  "Bananas are curved because they grow towards the sun against gravity.",
+  "A single cloud can weigh more than 1 million pounds.",
+  "Wombat cubes are shaped like cubes so they don't roll away."
+];
